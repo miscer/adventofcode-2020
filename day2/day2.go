e92caa5321cbba77f83be2e19f24c5268a99200a
Michal Miškerník <michal@miskernik.sk>
2020-12-02 21:10:41 +0100

    Day 2, first part

---
+++ day2/day2.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	flag.Parse()
+
+	filename := flag.Arg(0)
+	if filename == "" {
+		log.Fatal("Input file name is missing")
+	}
+
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("Failed opening file: %v", err)
+	}
+	defer file.Close()
+
+	entries, err := readEntries(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	count := 0
+	for _, e := range entries {
+		if e.isValid() {
+			count++
+		}
+	}
+
+	fmt.Printf("Found %d valid entries", count)
+}
+
+type entry struct {
+	letter   string
+	min      int
+	max      int
+	password string
+}
+
+func readEntries(file *os.File) (entries []entry, err error) {
+	s := bufio.NewScanner(file)
+
+	for s.Scan() {
+		line := s.Text()
+
+		entry, err := parseEntry(line)
+		if err != nil {
+			return nil, err
+		}
+
+		entries = append(entries, entry)
+	}
+
+	if err = s.Err(); err != nil {
+		return nil, err
+	}
+
+	return
+}
+
+func parseEntry(input string) (en entry, err error) {
+	_, err = fmt.Sscanf(input, "%d-%d %1s: %s", &en.min, &en.max, &en.letter, &en.password)
+	return
+}
+
+func (e *entry) isValid() bool {
+	c := strings.Count(e.password, e.letter)
+	return c >= e.min && c <= e.max
+}
