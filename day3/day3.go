b1bbeda7bb3c8ce4110c05ad129266fd48b772b7
Michal Miškerník <michal@miskernik.sk>
2020-12-05 21:42:46 +0100

    Day 3, first part

---
+++ day3/day3.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	flag.Parse()
+
+	filename := flag.Arg(0)
+	if filename == "" {
+		log.Fatal("Input file name is missing")
+	}
+
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("Failed opening file: %v", err)
+	}
+	defer file.Close()
+
+	m, err := readMap(file)
+	if err != nil {
+		log.Fatalf("Failed reading map: %v", err)
+	}
+
+	c, err := countTrees(m, 3, 1)
+	if err != nil {
+		log.Fatalf("Failed counting trees: %v", err)
+	}
+
+	fmt.Printf("Found %d trees\n", c)
+}
+
+type mapSquare int8
+
+const (
+	treeSquare mapSquare = iota
+	emptySquare
+)
+
+func (s mapSquare) String() string {
+	switch s {
+	case treeSquare:
+		return "🎄"
+	case emptySquare:
+		return "🌿"
+	default:
+		return "🤷"
+	}
+}
+
+type worldMap struct {
+	squares [][]mapSquare
+}
+
+func (m *worldMap) square(x int, y int) (mapSquare, error) {
+	if y >= len(m.squares) || y < 0 {
+		return emptySquare, fmt.Errorf("y coordinate out of range: %d", y)
+	}
+
+	row := m.squares[y]
+	return row[x%len(row)], nil
+}
+
+func (m *worldMap) height() int {
+	return len(m.squares)
+}
+
+func readMap(file *os.File) (m *worldMap, err error) {
+	m = &worldMap{}
+	s := bufio.NewScanner(file)
+
+	for s.Scan() {
+		line := s.Text()
+
+		var row []mapSquare
+		for _, ch := range line {
+			switch ch {
+			case '#':
+				row = append(row, treeSquare)
+			case '.':
+				row = append(row, emptySquare)
+			}
+		}
+
+		m.squares = append(m.squares, row)
+	}
+
+	err = s.Err()
+
+	return
+}
+
+func countTrees(m *worldMap, dx int, dy int) (int, error) {
+	count := 0
+	x := 0
+
+	for y := 0; y < m.height(); y += dy {
+		s, err := m.square(x, y)
+		if err != nil {
+			return 0, err
+		}
+
+		if s == treeSquare {
+			count++
+		}
+
+		x += dx
+	}
+
+	return count, nil
+}
